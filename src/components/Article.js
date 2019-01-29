9d8bee225a299330d65f71c1c5dd023f83154a53
Fgyter <fgyterr@yandex.ru>
2019-01-29 13:47:00 +0300

    Сonnecting styles and components

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article(props) {
-  const {article} = props
-  console.log('---', props)
-  const body = <section>{article.text}</section>
-  return (
-    <div className='hello' style={{color: 'green'}}>
-      <h2>{article.title}</h2>
-      {body}
-      <h3>creation date: {(new Date(article.date)). toDateString()}</h3>
-    </div>
-  )
+class Article extends Component {
+  state = {
+    isOpen: true
+  }
+
+  render() {
+    const {article} = this.props
+    console.log('---', this.props)
+    const style = {width: '75%'}
+    const body = this.state.isOpen && <section>{article.text}</section>
+    return (
+      <div className="card mx-auto" style = {style}>
+        <div className="card-header">
+          <h2>
+            {article.title}
+            <button onClick={this.handleClick} className="btn btn-primary float-right">
+              {this.state.isOpen ? 'cloze' : 'open'}
+            </button>
+          </h2>
+        </div>
+          <div className="card-body">
+            <h6 className="card-subtitle text-muted">
+              creation date: {(new Date(article.date)). toDateString()}
+            </h6>
+            {body}
+          </div>
+      </div>
+    )
+  }
+
+
+  handleClick = () => {
+    console.log('---', 'clicked')
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
 }
 
 export default Article
